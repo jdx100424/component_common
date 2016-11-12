2c919f82adf3aa04e380145e43b878191ed6fc43
15914380151@139.com <15914380151@139.com>
2016-11-13 00:33:23 +0800

    kafka java test

---
+++ src/main/java/com/maoshen/component/kafka/TestVO.java
@@ -0,0 +1,22 @@
+package com.maoshen.component.kafka;
+
+public class TestVO {
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
