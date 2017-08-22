fb3bf2f5e252a01e3826c162317c62cebe7a55af
daxian.jiang@lifesense.com <daxian.jiang@lifesense.com>
2017-08-22 14:50:01 +0800

    自动注册echoInfo

---
+++ src/main/java/com/maoshen/component/rest/echo/EchoInfoController.java
@@ -0,0 +1,99 @@
+package com.maoshen.component.rest.echo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/echoInfo")
+public class EchoInfoController implements ApplicationContextAware{
+	/**
+	<bean id="sqlSessionTemplate" class="org.mybatis.spring.SqlSessionTemplate">  
+        <property name="sqlSessionFactory" ref="sqlSessionFactory" />  
+   		org.mybatis.spring.SqlSessionFactoryBean
+    </bean>  
+	 
+	 */
+	
+	private static final String DB = "DB";
+	private static final String REDIS = "REDIS";
+	private static final String IS_NEED_CHECK = "0";
+	private static final String SUCCESS = "200";
+	private static final Logger LOGGER = LoggerFactory.getLogger(EchoInfoController.class);
+
+	private ApplicationContext applicationContext;
+	
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "", method = { RequestMethod.GET })
+	@ResponseBody
+	protected Map<String, Object> echoInfo(HttpServletRequest request) {
+		long echoStart = System.currentTimeMillis();
+		//最终检测结果
+		boolean result = true;
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		if (StringUtils.isNotBlank(request.getParameter(DB)) && IS_NEED_CHECK.equals(request.getParameter(DB))) {
+			Map<String, org.mybatis.spring.SqlSessionTemplate> sqlSessionTemplateMap = applicationContext.getBeansOfType(org.mybatis.spring.SqlSessionTemplate.class);
+			if(sqlSessionTemplateMap==null || sqlSessionTemplateMap.isEmpty()){
+				map.put(DB, SUCCESS + ",xml not fount");
+			}else{
+				try{
+					sqlSessionTemplateMap.get(0).selectOne("select 1");
+					map.put(DB, SUCCESS);
+				}catch(Exception e){
+					LOGGER.error(e.getMessage(),e);
+					map.put(DB, e.getMessage());
+					result = false;
+				}
+			}
+		} else {
+			map.put(DB, SUCCESS + ",not check");
+		}
+		
+		if (StringUtils.isNotBlank(request.getParameter(REDIS)) && IS_NEED_CHECK.equals(request.getParameter(DB))) {
+			Map<String, RedisTemplate> redisTemplateMap = applicationContext.getBeansOfType(org.springframework.data.redis.core.RedisTemplate.class);
+			if(redisTemplateMap==null || redisTemplateMap.isEmpty()){
+				map.put(REDIS, SUCCESS + ",xml not fount");
+			}else{
+				try{
+					redisTemplateMap.get(0).opsForValue().get("test");
+					map.put(REDIS, SUCCESS);
+				}catch(Exception e){
+					LOGGER.error(e.getMessage(),e);
+					map.put(REDIS, e.getMessage());
+					result = false;
+				}
+			}
+		} else {
+			map.put(REDIS, SUCCESS + ",not check");
+		}
+
+		// log级别
+		map.put("logger_debug", LOGGER.isDebugEnabled());
+		map.put("logger_info", LOGGER.isInfoEnabled());
+		map.put("logger_warn", LOGGER.isWarnEnabled());
+		map.put("logger_error", LOGGER.isErrorEnabled());
+
+		map.put("runTime", System.currentTimeMillis() - echoStart);
+		map.put("result", result);
+		return map;
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+}
--- src/main/java/com/maoshen/component/spring/BaseContextLoaderListener.java
+++ src/main/java/com/maoshen/component/spring/BaseContextLoaderListener.java
@@ -2,13 +2,27 @@ package com.maoshen.component.spring;
 
 import javax.servlet.ServletContextEvent;
 
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import com.maoshen.component.base.util.ServerLauncherStatus;
+import com.maoshen.component.rest.echo.EchoInfoController;
 
 
 public class BaseContextLoaderListener extends org.springframework.web.context.ContextLoaderListener{
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		super.contextInitialized(event);
+		
+		//自动注册echoInfoController
+		WebApplicationContext applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(event.getServletContext());
+		DefaultListableBeanFactory acf = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
+		String beanName = EchoInfoController.class.getName();
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(EchoInfoController.class);
+		acf.registerBeanDefinition(beanName, builder.getRawBeanDefinition());
+
 		ServerLauncherStatus.get().started();//启动完成标志
 	}
 }
