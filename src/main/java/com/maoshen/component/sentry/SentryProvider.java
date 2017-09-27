6378e266a8587dc941e6e99eea7423af0ee38523
daxian.jiang@lifesense.com <daxian.jiang@lifesense.com>
2017-09-27 11:26:49 +0800

    1

--- src/main/java/com/maoshen/component/sentry/SentryProvider.java
+++ src/main/java/com/maoshen/component/sentry/SentryProvider.java
@@ -11,25 +11,39 @@ import io.sentry.event.EventBuilder;
 public class SentryProvider {
 	private static boolean isInit = false;
 	private static final String PROJECT_NAME = "projectName";
-	public static void sendLog(String projectName,String message,io.sentry.event.Event.Level event,org.slf4j.Logger logger) {
-		sendLog(projectName,message,event,logger,null);
+
+	public static void sendLog(String projectName, String message, io.sentry.event.Event.Level event,
+			org.slf4j.Logger logger) {
+		sendLog(projectName, message, event, logger, null);
 	}
-	public static void sendLog(String projectName,String message,io.sentry.event.Event.Level event,org.slf4j.Logger logger,Exception e) {
-		if(isInit){
-			EventBuilder eventBuilder = new EventBuilder()
-                    .withMessage(message)
-                    .withLevel(event)
-                    .withLogger(logger.getName());
+
+	public static void sendLog(String projectName, String message, io.sentry.event.Event.Level event,
+			org.slf4j.Logger logger, Exception e) {
+		if (isInit) {
+			//EventBuilder eventBuilder = new EventBuilder().withMessage(message).withLevel(event).withLogger(logger.getName());
 			Sentry.getContext().addTag(PROJECT_NAME, projectName);
-			if(e!=null){
+			if (e != null) {
 				Sentry.getContext().addExtra("exception", ExceptionUtils.getStackTrace(e));
 			}
-			Sentry.capture(eventBuilder);
+			switch (event) {
+			case DEBUG:
+				logger.debug(message);
+			case INFO:
+				logger.info(message);
+			case WARNING:
+				logger.warn(message);
+			case ERROR:
+				logger.error(message);
+			default:
+				logger.error(message);
+			}
+			// Sentry.capture(eventBuilder);
 		}
 	}
-	public static void init(){
-		String dsn = ResourceUtils.get("dsn" , "");
-		if(StringUtils.isNotBlank(ResourceUtils.get("dsn" , ""))){
+
+	public static void init() {
+		String dsn = ResourceUtils.get("dsn", "");
+		if (StringUtils.isNotBlank(ResourceUtils.get("dsn", ""))) {
 			Sentry.init(dsn);
 			isInit = true;
 		}
